6f8602172a872af8ab98b74869e981ce3885fed9
John Estabrook <jestabro@vyos.io>
2024-09-06 19:41:00 -0500

    curly_parser_test for completness; this obscures explicit terminators

--- test/curly_parser_test.ml
+++ test/curly_parser_test.ml
@@ -1,6 +1,9 @@
+[@@@ocaml.warning "-27"]
+
 open OUnit2
 
-module CT = Config_tree
+module CT = Vyos1x.Config_tree
+module VT = Vyos1x.Vytree
 
 let config_empty = ""
 
@@ -8,30 +11,30 @@ let config_trivial = "foo { }"
 
 let config_nested = "foo { bar { } baz { } }"
 
-let config_leaf_top_level = "foo bar;"
+let config_leaf_top_level = "foo bar"
 
-let config_tag_top_level = "foo bar { baz quux; }"
+let config_tag_top_level = "foo bar { baz quux\n }"
 
-let config_with_leaf = "foo { bar baz; }"
-let config_with_leaf_url_unquoted = "foo { bar http://www2.example.org/foo; }"
-let config_with_leaf_value_quoted = "foo { bar \"foo bar\"; }"
-let config_with_leaf_value_single_quoted = "foo { bar \'foo bar\'; }"
-let config_with_leaf_valueless = "foo { bar; }"
+let config_with_leaf = "foo { bar baz }"
+let config_with_leaf_url_unquoted = "foo { bar http://www2.example.org/foo }"
+let config_with_leaf_value_quoted = "foo { bar \"foo bar\" }"
+let config_with_leaf_value_single_quoted = "foo { bar \'foo bar\' }"
+let config_with_leaf_valueless = "foo { bar }"
 
 (* XXX: naive use of Menhir's separated_list doesn't allow [baz; xyzzy;],
         perhaps we should support it too *)
-let config_with_multi = "foo { bar [baz; xyzzy]; }"
+let config_with_multi = "foo { bar [baz; xyzzy] }"
 
-let config_with_tag = "foo { bar baz { quux xyzzy; } bar qwerty { quux foobar; } }"
+let config_with_tag = "foo { bar baz { quux xyzzy } bar qwerty { quux foobar } }"
 let config_with_tag_nonalpha = "foo { bar baz0.99 { } bar baz1-8 { } }"
 
 let config_with_comment = "foo { /* comment */ bar { } }"
-let config_with_leaf_node_comment = "foo { /* comment */ bar baz; }"
+let config_with_leaf_node_comment = "foo { /* comment */ bar baz }"
 let config_with_tag_node_comment = "foo { /* comment */ bar baz { } }"
 
 let config_with_duplicate_node = "foo { bar { baz {} } bar { baz {} } }"
 let config_with_duplicate_tag_node = "foo { bar baz0 { } bar baz0 { } }"
-let config_with_duplicate_leaf_node = "foo { bar baz; bar quux; }"
+let config_with_duplicate_leaf_node = "foo { bar \"baz\" bar \"quux\" }"
 
 let config_with_inactive_node = "#INACTIVE foo { bar baz; }"
 let config_with_inactive_leaf_node = "foo { #INACTIVE bar baz; quux xyzzy; }"
@@ -44,22 +47,26 @@ let config_with_ephemeral_tag_node = "foo { #EPHEMERAL bar baz { #INACTIVE quux 
 let config_with_inactive_and_ephemeral_node = "#INACTIVE #EPHEMERAL foo { bar { } }"
 let config_with_inactive_node_and_comment = "/* comment */ #INACTIVE #EPHEMERAL foo { }"
 
-let parse s = Curly_parser.config Curly_lexer.token (Lexing.from_string s)
+let parse s = Vyos1x.Parser.from_string s
+
+let raises_parsing_exception f =
+    try ignore @@ f (); false
+    with Vyos1x.Util.Syntax_error _ -> true
 
 (* Empty config is considered valid, creates just the root node *)
 let test_parse_empty test_ctxt =
     let config = parse config_empty in
-    assert_equal (Vytree.list_children config) []
+    assert_equal (VT.list_children config) []
 
 (* A config with just an empty node is considered valid too. Not sure if it should! *)
 let test_parse_trivial test_ctxt =
     let config = parse config_trivial in
-    assert_equal (Vytree.list_children config) ["foo"]
+    assert_equal (VT.list_children config) ["foo"]
 
 (* A config with nested nodes is parser correctly *)
 let test_parse_nested test_ctxt =
     let config = parse config_nested in 
-    assert_equal (Vytree.get config ["foo"] |> Vytree.list_children ) ["bar"; "baz"]
+    assert_equal (VT.get config ["foo"] |> VT.list_children ) ["bar"; "baz"]
 
 (* Leaf nodes are parsed correctly *)
 let test_parse_with_leaf test_ctxt =
@@ -84,15 +91,19 @@ let test_parse_with_leaf_value_single_quoted test_ctxt =
 (* Valueless leaf nodes work *)
 let test_parse_with_leaf_valueless test_ctxt =
     let config = parse config_with_leaf_valueless in
-    assert_equal (Vytree.get config ["foo"] |> Vytree.list_children) ["bar"]
+    assert_equal (VT.get config ["foo"] |> VT.list_children) ["bar"]
+
+(* N.B. the following two cases _are_ allowed: cf. nosetests vyos-1x *)
 
 (* Top level leaf nodes are not allowed *)
 let test_parse_top_level_leaf_node test_ctxt =
-    assert_raises Curly_parser.Error (fun () -> parse config_leaf_top_level)
+    assert_bool "Top level leaf nodes was not caught by parser"
+      (raises_parsing_exception (fun () -> parse config_leaf_top_level))
 
 (* Top level tag nodes are not allowed *)
 let test_parse_top_level_tag_node test_ctxt =
-    assert_raises Curly_parser.Error (fun () -> parse config_tag_top_level)
+    assert_bool "Top level tag node was not caught by parser"
+      (raises_parsing_exception (fun () -> parse config_tag_top_level))
 
 (* Nodes with multiple values are handled correctly *)
 let test_parse_with_multi test_ctxt =
@@ -117,14 +128,14 @@ let test_parse_with_tag_node_comment test_ctxt =
 (* Tag nodes are parsed correctly *)
 let test_parse_with_tag test_ctxt =
     let config = parse config_with_tag in
-    assert_equal (Vytree.get config ["foo"; "bar"] |> Vytree.list_children) ["baz"; "qwerty"];
+    assert_equal (VT.get config ["foo"; "bar"] |> VT.list_children) ["baz"; "qwerty"];
     assert_equal (CT.get_value config ["foo"; "bar"; "baz"; "quux"]) "xyzzy";
     assert_equal (CT.get_value config ["foo"; "bar"; "qwerty"; "quux"]) "foobar"
 
 (* Non-alphanumeric characters are allowed in tag nodes *)
 let test_parse_with_tag_nonalpha test_ctxt =
     let config = parse config_with_tag_nonalpha in
-    assert_equal (Vytree.get config ["foo"; "bar"] |> Vytree.list_children) ["baz0.99"; "baz1-8"]
+    assert_equal (VT.get config ["foo"; "bar"] |> VT.list_children) ["baz0.99"; "baz1-8"]
 
 (* Normal nodes with duplicate children are detected *)
 let test_parse_node_duplicate_child test_ctxt =
@@ -142,6 +153,7 @@ let test_parse_duplicate_leaf_node test_ctxt =
     let config = parse config_with_duplicate_leaf_node in
     assert_equal (CT.get_values config ["foo"; "bar"]) ["baz"; "quux"]
 
+(*
 (* Inactive nodes are parsed correctly *)
 let test_parse_inactive_node test_ctxt =
     let config = parse config_with_inactive_node in
@@ -189,6 +201,7 @@ let test_parse_inactive_node_and_comment test_ctxt =
     assert_equal (CT.is_ephemeral config ["foo"]) true;
     assert_equal (CT.is_inactive config ["foo"]) true;
     assert_equal (CT.get_comment config ["foo"]) (Some "comment")
+*)
 
 let suite =
     "VyConf curly config parser tests" >::: [
@@ -211,14 +224,6 @@ let suite =
         "test_parse_node_duplicate_child" >:: test_parse_node_duplicate_child;
         "test_parse_tag_node_duplicate_child" >:: test_parse_tag_node_duplicate_child;
         "test_parse_duplicate_leaf_node" >:: test_parse_duplicate_leaf_node;
-        "test_parse_inactive_node" >:: test_parse_inactive_node;
-        "test_parse_inactive_leaf_node" >:: test_parse_inactive_leaf_node;
-        "test_parse_inactive_tag_node" >:: test_parse_inactive_tag_node;
-        "test_parse_ephemeral_node" >:: test_parse_ephemeral_node;
-        "test_parse_ephemeral_leaf_node" >:: test_parse_ephemeral_leaf_node;
-        "test_parse_ephemeral_tag_node" >:: test_parse_ephemeral_tag_node;
-        "test_parse_inactive_and_ephemeral_node" >:: test_parse_inactive_and_ephemeral_node;
-        "test_parse_inactive_node_and_comment" >:: test_parse_inactive_node_and_comment;
     ]
 
 let () =
