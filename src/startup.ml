95f885ec8c5786272a3bc0189799356ccdfb291c
John Estabrook <jestabro@vyos.io>
2024-09-13 23:12:13 -0500

    fix error in call to Internal.read_internal

--- src/startup.ml
+++ src/startup.ml
@@ -134,8 +134,6 @@ module I = Vyos1x.Internal.Make(Vyos1x.Reference_tree)
 
 let read_reference_tree file =
     try
-        let chan = open_in file in
-        let s = really_input_string chan (in_channel_length chan) in
-        let reftree = I.read_internal s in
+        let reftree = I.read_internal file in
         Ok reftree
     with Sys_error msg -> Error msg
