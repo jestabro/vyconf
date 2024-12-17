9849648f4d88e3b9b7e6313f0790160bf3bd39ee
John Estabrook <jestabro@vyos.io>
2024-12-16 22:33:44 -0600

    T6946: calculate priority lists

--- src/commit.ml
+++ src/commit.ml
@@ -1,5 +1,6 @@
 module VT = Vyos1x.Vytree
 module CT = Vyos1x.Config_tree
+module CD = Vyos1x.Config_diff
 module RT = Vyos1x.Reference_tree
 
 type commit_data = {
@@ -46,7 +47,7 @@ let owner_args_from_data p s =
     | None -> None, None
     | Some o ->
     let oa = Pcre.split o in
-    let owner = List.nth oa 0 in
+    let owner = FilePath.basename (List.nth oa 0) in
     if List.length oa < 2 then Some owner, None
     else
     let var = List.nth oa 1 in
@@ -85,7 +86,7 @@ let get_commit_data rt ct (path, cs') t =
                    script = own;
                    priority = priority;
                    arg_value = arg;
-                   path = path; }
+                   path = rpath; }
     in
     let tag_values =
         match RT.is_tag rt rt_path with
@@ -98,12 +99,41 @@ let get_commit_data rt ct (path, cs') t =
         | _ -> List.fold_left (add_tag_instance c_data) cs' tag_values
     in (path, cs)
 
+let get_commit_set rt ct =
+    snd (VT.fold_tree_with_path (get_commit_data rt ct) ([], CS.empty) ct)
+
+(* adjust for legacy order: del list only if owner node or tag value *)
+let legacy_order del_t a b =
+    let shift c_data (c_del, c_add) =
+        let path =
+            match c_data.tag_value with
+            | None -> c_data.path
+            | Some v -> c_data.path @ [v]
+        in
+        match VT.is_terminal_path del_t path with
+        | false -> CS.remove c_data c_del, CS.add c_data c_add
+        | true -> c_del, c_add
+    in
+    CS.fold shift a (a, b)
+
+let calculate_priority_lists rt at wt =
+    let diff = CD.diff_tree [] at wt in
+    let del_tree = CT.get_subtree diff ["del"] in
+    let add_tree = CT.get_subtree diff ["add"] in
+    let cs_del' = get_commit_set rt del_tree in
+    let cs_add' = get_commit_set rt add_tree in
+    let cs_del, cs_add = legacy_order del_tree cs_del' cs_add' in
+    List.rev (CS.elements cs_del), CS.elements cs_add
+
 let test_commit_data rt ct =
-    let cs =
-        VT.fold_tree_with_path (get_commit_data rt ct) ([], CS.empty) ct
+    let at =
+        CT.make ""
+    in
+    let _, cs =
+        calculate_priority_lists rt at ct
     in
     let sprint_commit_data acc s =
         acc ^ "\n" ^ show_commit_data s
     in
-    let out = List.fold_left sprint_commit_data "" (CS.elements (snd cs))
+    let out = List.fold_left sprint_commit_data "" cs
     in print_endline out
