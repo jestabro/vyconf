ebd54a613d30b7a6f5d7001d853eeb007f71e782
John Estabrook <jestabro@vyos.io>
2025-02-21 23:01:48 -0600

    simplification

--- src/vycall_client.ml
+++ src/vycall_client.ml
@@ -14,6 +14,11 @@ type t = {
     oc: Lwt_io.output Lwt_io.channel;
 }
 
+(* explicit translation between commit data and commit proto
+ * to keep the commit data opaque to the Python commit daemon.
+ * Mutability on the Python side updates the commit data with
+ * results of script execution in reply field.
+ *)
 let node_data_to_call nd =
     { script_name = nd.script_name;
       tag_value = nd.tag_value;
@@ -22,19 +27,9 @@ let node_data_to_call nd =
     }
 
 let call_to_node_data ((c: call), nd) =
-    let base =
-    { script_name = c.script_name;
-      priority = nd.priority;
-      tag_value = c.tag_value;
-      arg_value = c.arg_value;
-      path = nd.path;
-      source = nd.source;
-      out = "";
-    }
-    in
     match c.reply with
-    | None -> base
-    | Some r -> { base with out = r.out; }
+    | None -> nd
+    | Some r -> { nd with out = r.out; }
 
 let commit_data_to_commit_proto cd =
     { session_id = cd.session_id;
@@ -47,16 +42,12 @@ let commit_data_to_commit_proto cd =
     }
 
 let commit_proto_to_commit_data (c: commit) cd =
-    { session_id = c.session_id;
-      named_active = c.named_active;
-      named_proposed = c.named_proposed;
-      dry_run = c.dry_run;
-      atomic = c.atomic;
-      background = c.background;
+    { cd with
       node_list =
           List.map call_to_node_data (List.combine c.calls cd.node_list);
     }
 
+(* read/write message from/to socket *)
 let call_write oc msg =
     let length = Bytes.length msg in
     let length' = Int32.of_int length in
@@ -75,6 +66,7 @@ let call_read ic =
     let%lwt () = Lwt_io.read_into_exactly ic buffer 0 length in
     Lwt.return buffer
 
+(* encode/decode commit data *)
 let do_call client request =
     let enc = Pbrt.Encoder.create () in
     let () = encode_pb_commit request enc in
@@ -83,6 +75,7 @@ let do_call client request =
     let%lwt resp = call_read client.ic in
     decode_pb_commit (Pbrt.Decoder.of_bytes resp) |> Lwt.return
 
+(* socket management and commit callback *)
 let create sockfile =
     let open Lwt_unix in
     let sock = socket PF_UNIX SOCK_STREAM 0 in
@@ -112,6 +105,7 @@ let do_commit session_data =
         update (commit_proto_to_commit_data resp session_data)
     in Lwt_main.run @@ run ()
 
+(* test function *)
 let test_commit at wt =
     let vc =
         ST.load_daemon_config DF.defaults.config_file in
